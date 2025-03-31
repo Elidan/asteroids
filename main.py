20a8ad146bc71bf0f4efa0a33017088bb95106dc
Elidan <elidan1012@gmail.com>
2025-03-31 23:15:51 +0200

    player added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,15 +9,26 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        # Update player
+        player.update(dt)
 
         # Paint background black
         pygame.Surface.fill(screen, color="black")
 
+        # Paint the player
+        player.draw(screen)
+
+        #Get delta time - 60 FPS
+        dt = clock.tick(60) / 1000
 
         # should always be at the end
         pygame.display.flip()
